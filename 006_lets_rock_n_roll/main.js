ef7bdadaf8331c518ac1a19ae655116988337480
Alexander <alexander.a.kelso@gmail.com>
2023-08-25 09:47:06 +1000

    updated javascript, commented out the old bootstrap load

--- 006_lets_rock_n_roll/main.js
+++ 006_lets_rock_n_roll/main.js
@@ -1,53 +1,55 @@
 // #################################### CAROUSEL CODE
 
-const prevBtn = document.querySelector('.carousel-prev');
-const nextBtn = document.querySelector('.carousel-next');
-const content = document.querySelector('.carousel-content');
-
-let logosOnScreen = 4; // Default value for larger screens
-
-window.addEventListener('resize', () => {
-    // Update the number of logos displayed based on the screen size.
-    if (window.innerWidth <= 600) {
-        logosOnScreen = 2;
-    } else if (window.innerWidth <= 900) {
-        logosOnScreen = 3;
-    } else {
-        logosOnScreen = 4;
+document.addEventListener('DOMContentLoaded', function() {
+    const prevBtn = document.querySelector('.carousel-prev');
+    const nextBtn = document.querySelector('.carousel-next');
+    const content = document.querySelector('.carousel-content');
+    
+    let logosOnScreen = 4; // Default value for larger screens
+    
+    window.addEventListener('resize', () => {
+        // Update the number of logos displayed based on the screen size.
+        if (window.innerWidth <= 600) {
+            logosOnScreen = 2;
+        } else if (window.innerWidth <= 900) {
+            logosOnScreen = 3;
+        } else {
+            logosOnScreen = 4;
+        }
+        checkButtons();
+    });
+    
+    prevBtn.addEventListener('click', () => {
+        content.scrollLeft -= content.clientWidth; // Move by the visible width of the carousel.
+        checkButtons();
+    });
+    
+    nextBtn.addEventListener('click', () => {
+        content.scrollLeft += content.clientWidth; // Move by the visible width of the carousel.
+        checkButtons();
+    });
+    
+    // Disable/enable the buttons based on the scroll position.
+    function checkButtons() {
+        const maxScroll = content.scrollWidth - content.clientWidth;
+    
+        if (content.scrollLeft <= 0) {
+            prevBtn.disabled = true;
+        } else {
+            prevBtn.disabled = false;
+        }
+    
+        if (content.scrollLeft >= maxScroll - (content.clientWidth * 0.1)) { // added a buffer
+            nextBtn.disabled = true;
+        } else {
+            nextBtn.disabled = false;
+        }
     }
-    checkButtons();
+    
+    // Initialize with the current setting.
+    window.dispatchEvent(new Event('resize'));
+    
 });
-
-prevBtn.addEventListener('click', () => {
-    content.scrollLeft -= content.clientWidth; // Move by the visible width of the carousel.
-    checkButtons();
-});
-
-nextBtn.addEventListener('click', () => {
-    content.scrollLeft += content.clientWidth; // Move by the visible width of the carousel.
-    checkButtons();
-});
-
-// Disable/enable the buttons based on the scroll position.
-function checkButtons() {
-    const maxScroll = content.scrollWidth - content.clientWidth;
-
-    if (content.scrollLeft <= 0) {
-        prevBtn.disabled = true;
-    } else {
-        prevBtn.disabled = false;
-    }
-
-    if (content.scrollLeft >= maxScroll - (content.clientWidth * 0.1)) { // added a buffer
-        nextBtn.disabled = true;
-    } else {
-        nextBtn.disabled = false;
-    }
-}
-
-// Initialize with the current setting.
-window.dispatchEvent(new Event('resize'));
-
 
 
 // #################################### ARTICLE CAROUSEL CONTENT
